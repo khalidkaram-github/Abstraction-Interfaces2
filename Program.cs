b9fd93ac2198d651fd909f9760fe80c7fcf8f67f
khalid karam <khalid.tasks@gmail.com>
2024-09-20 23:39:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Abstraction_Interfaces2
+{
+    //interface with default implementation
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Dog();
+            dog.MakeSound();
+            dog.Move();
+        }
+    }
+
+    public interface IAnimal
+    {
+        // Method with default implementation
+        void MakeSound()
+        {
+            Console.WriteLine("The animal makes a sound.");
+        }
+
+        // Method without default implementation (abstract)
+        void Move();
+    }
+    public class Dog : IAnimal
+    {
+        // Override the default implementation
+        public void MakeSound()
+        {
+            Console.WriteLine("The dog barks.");
+        }
+
+        // Must implement the abstract method
+        public void Move()
+        {
+            Console.WriteLine("The dog runs.");
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        // Use the default implementation of MakeSound
+        // Must implement the abstract method
+        public void Move()
+        {
+            Console.WriteLine("The cat moves stealthily.");
+        }
+    }
+
+
+}
